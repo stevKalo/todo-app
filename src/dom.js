201c129f8e68e6fb91944bf836ca255fed3502cc
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2023-12-19 12:29:54 -0500

    Add createItem function to create and append todo items

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,7 @@
-export default function createNode(element) {
-  return document.createElement(element);
+export default function createItem(obj, parent) {
+  const item = document.createElement("li");
+  item.classList.add("item");
+  item.classList.add(`priority-${obj.priority}`);
+  item.textContent = obj.title;
+  parent.appendChild(item);
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import TodoItem from "./items.js";
+import createItem from "./dom.js";
 
 // Global Variables
 const content = document.getElementById("content");
 const addBtn = document.getElementById("add-btn");
 const dialog = document.getElementById("todo-modal");
 const saveBtn = document.getElementById("save-btn");
+const taskList = document.getElementById("tasks");
 
 // Todo List Array
 let todoList = [];
@@ -41,7 +43,7 @@ saveBtn.addEventListener("click", () => {
     false
   );
   todoList.push(todoItem);
-  console.log(todoItem);
   dialog.close();
   modalClear();
+  createItem(todoItem, taskList);
 });
